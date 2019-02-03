34674e88a95bd24d59667bdc058659ee55de88a3
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-02-03 16:20:24 -0200

    encode e decode ainda sem parametros

--- scripts/cipher.js
+++ scripts/cipher.js
@@ -52,7 +52,26 @@ function encode (){
 
 // Reconstruir decode
 
+function decode(){
+  let mensagem = document.getElementById('resultado').value;
+  console.log(mensagem)
+  let chave = parseInt(document.getElementById('chave').value);
+  let alfabeto = 26;
+  let result = ""
 
+  for (i = 0; i < mensagem.length; i++){
+    let getChar = mensagem.charCodeAt(i);
+    let encode = (getChar - 97) - chave % alfabeto + 97;
+    let asc = String.fromCharCode(encode);
+
+     console.log(asc)
+     
+
+    
+    let publicar = document.getElementById("copyArea").innerHTML = result += asc;
+    console.log(result)
+  }
+}
 
 
 
