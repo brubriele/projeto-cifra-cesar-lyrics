e578079b7a27e97ef4f8e479acb2795eb3c890f9
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-02-06 14:56:50 -0200

    Parametrização de cipher e construção de tests.js

--- scripts/cipher.js
+++ scripts/cipher.js
@@ -1,65 +1,47 @@
 
-function encode (){
+function getEncode(){
   let mensagem = document.getElementById('msg').value;
   let chave = parseInt(document.getElementById('chave').value);
+  let encodeResult = encode(mensagem, chave); document.getElementById('resultado').value = encodeResult;
+}
+
+function getDecode(){
+  let resultado = document.getElementById('resultado').value;
+  let chave = parseInt(document.getElementById('chaveDecode').value);
+  let decodeResult = decode(resultado, chave);
+  document.getElementById("copyArea").value = decodeResult;
+}
+
+
+function encode(mensagem, chave){
+  
   let alfabeto = 26;
   let result = ""
 
   for (i = 0; i < mensagem.length; i++){
     let getChar = mensagem.charCodeAt(i);
     let encode = (getChar - 97) + chave % alfabeto + 97;
+   
     let asc = String.fromCharCode(encode);
-    let publicar = document.getElementById('resultado').innerHTML = result += asc;
+    result = result + asc;
+    
    
   }
+  return result
 }
 
-function decode(){
-  let mensagem = document.getElementById('resultado').value;
-  let chave = parseInt(document.getElementById('chaveDecode').value);
+
+function decode(resultado, chave){
+
   let alfabeto = 26;
   let result = ""
 
-  for (i = 0; i < mensagem.length; i++){
-    let getChar = mensagem.charCodeAt(i);
+  for (i = 0; i < resultado.length; i++){
+    let getChar = resultado.charCodeAt(i);
     let encode = (getChar - 97) - chave % alfabeto + 97;
     let asc = String.fromCharCode(encode);
-    let publicar = document.getElementById("copyArea").innerHTML = result += asc;
+     result = result + asc;
+    
   }
+  return result
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- scripts/tests.js
+++ scripts/tests.js
@@ -1,31 +1,3 @@
-function encode (mensagem, chave){
-  
-  let alfabeto = 26;
-  let result = ""
-
-  for (i = 0; i < mensagem.length; i++){
-    let getChar = mensagem.charCodeAt(i);
-    let encode = (getChar - 97) + chave % alfabeto + 97;
-    let asc = String.fromCharCode(encode);
-    let publicar = result += asc;
-  }
-}
-
-function decode(resultado, chave){
-  
-  let alfabeto = 26;
-  let result = ""
-
-  for (i = 0; i < resultado.length; i++){
-    let getChar = resultado.charCodeAt(i);
-    let encode = (getChar - 97) - chave % alfabeto + 97;
-    let asc = String.fromCharCode(encode);
-    let publicar = result += asc;
-  }
-}
-
-
-
 function teste(calculado, esperado) {
   if (calculado === esperado) {
     console.log("Passou!");
@@ -35,3 +7,5 @@ function teste(calculado, esperado) {
     return false;
   }
 }
+
+teste(encode('teste',27),'uftuf')
